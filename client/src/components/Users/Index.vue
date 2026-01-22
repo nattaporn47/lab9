2684323e4cfc793d4ae18167f2f145f77c25a586
nattapon kumyaito <nattaponk@nu.ac.th>
2026-01-22 15:46:48 +0700

    undo server authen

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -6,14 +6,28 @@
 </template>
 
 <script>
+import axios from "axios"; // Import axios เข้ามาใช้งานโดยตรง
+
 export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+
   // Logic จะเขียนตรงนี้
-    methods: {
-    navigateTo (route) {
-      this.$router.push(route)
-    }
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route);
+    },
+  },
+
+  async created(){
+    const response = await axios.get("http://localhost:8081/users")
+    this.users = response.data;
+    console.log("Fetched users:", this.users);
   }
-}
+};
 </script>
 
 <style scoped>
